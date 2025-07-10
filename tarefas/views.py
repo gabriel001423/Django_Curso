29a13dc81c093c5294d209e630e58c2156f35cc8
Gabriel Jesus <166547615+gabriel001423@users.noreply.github.com>
2025-07-09 21:08:51 -0300

    v1

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from tarefas.forms import TarefasForm
 from .models import Tarefas
@@ -8,7 +8,7 @@ def listaTarefas(request):
     tarefas_list = Tarefas.objects.all().order_by('-created_at')
     return render(request, 'tarefas/list.html', {'tarefas':tarefas_list})
 
-
+# funçao adiciona uma nova tarefa
 def novaTarefa(request):
     if request.method == 'POST':
         form = TarefasForm(request.POST)
@@ -18,3 +18,29 @@ def novaTarefa(request):
     else:
         form = TarefasForm()
         return render(request, 'tarefas/addTarefa.html', {'form':form})
+    
+# funçao pra edita a tarefa adcionada
+def tarefaView(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    return render(request, 'tarefas/tarefa.html', {'tarefa':tarefa})
+
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefasForm(request.POST)
+
+    if(request.method == 'POST'):
+        form = TarefasForm(request.POST, instance=tarefa)
+        
+        if(form.is_valid()):
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    else:
+        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+
+# funçao pra deleta a tarefa selecionada
+def deleteTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    tarefa.delete()
+    return redirect('/')
